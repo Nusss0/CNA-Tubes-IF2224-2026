49348bde2eb061506f0aaeab4db0c50b6c052ae2
Nusss0 <stevanus610@gmail.com>
2026-05-05 21:51:33 +0700

    feat : ADT Tree

---
+++ src/syntax/parse_tree.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "../std.hpp"
+
+struct ParseNode {
+    string label;
+    vector<shared_ptr<ParseNode>> children;
+
+    explicit ParseNode(const string& lbl) : label(lbl) {}
+};
+
+using NodePtr = shared_ptr<ParseNode>;
+
+NodePtr makeNode(const string& label);
+void    addChild(const NodePtr& parent, const NodePtr& child);
+
+void printTree(const NodePtr& root);
+void saveTreeToFile(const NodePtr& root, const string& path);
